6191efe1c39f63ed70d566e33b4b25db15efc449
Chestnut <li1928631@163.com>
2023-02-25 22:23:24 +0800

    添加编码器测速

---
+++ 06-9编码器测速/Hardware/Encoder.c
@@ -0,0 +1,45 @@
+#include "stm32f10x.h"                  // Device header
+
+
+void Encoder_Init(void)
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+    
+    TIM_InternalClockConfig(TIM3);
+    
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStruct.TIM_Period = 65536 - 1; // 防止溢出
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 1 - 1; // PSC
+    TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStruct);
+    
+    TIM_ICInitTypeDef TIM_ICInitStruct;
+    TIM_ICStructInit(&TIM_ICInitStruct);
+    TIM_ICInitStruct.TIM_Channel = TIM_Channel_1;
+    TIM_ICInitStruct.TIM_ICFilter = 0xF;
+    TIM_ICInit(TIM3, &TIM_ICInitStruct);
+    TIM_ICInitStruct.TIM_Channel = TIM_Channel_2;
+    TIM_ICInitStruct.TIM_ICFilter = 0xF;
+    TIM_ICInit(TIM3, &TIM_ICInitStruct);
+    
+    TIM_EncoderInterfaceConfig(TIM3, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+    
+    TIM_Cmd(TIM3, ENABLE);
+}
+
+int16_t Encoder_Get(void)
+{
+    int16_t Temp = TIM_GetCounter(TIM3);
+    TIM_SetCounter(TIM3, 0);
+    return Temp;
+}
+
